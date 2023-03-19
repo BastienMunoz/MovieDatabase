362a7a850f001b4afcecc81d895f24a6e9da5aa6
Bastien MUNOZ <bastien.munoz@outlook.com>
2023-03-19 13:15:58 +0100

    Fake data added for movie

--- app/src/test/java/com/ichi/moviedatabase/fake/data/FakeWSMovie.kt
+++ app/src/test/java/com/ichi/moviedatabase/fake/data/FakeWSMovie.kt
@@ -2,5 +2,26 @@ package com.ichi.moviedatabase.fake.data
 
 import com.ichi.moviedatabase.network.models.WSMovie
 
-// TODO: fill with actual data when testing GetMovieUseCase()
-val fakeWSMovie = WSMovie("", 0, listOf(), "", listOf(), "", 0.0, 0, listOf(), WSMovie.Status.CANCELED, "", 0.0, 0)
+val fakeMovie = WSMovie(
+    "https://fake-movie.com/backdrop.jpg",
+    1_000_000,
+    listOf(
+        WSMovie.Genre("Action"),
+        WSMovie.Genre("Drama"),
+    ),
+    "A film about something",
+    listOf(
+        WSMovie.Company("A company"),
+    ),
+    "2017-04-12",
+    10_000_000.0,
+    127,
+    listOf(
+        WSMovie.SpokenLanguage("French"),
+        WSMovie.SpokenLanguage("English"),
+    ),
+    WSMovie.Status.RELEASED,
+    "A title",
+    4.7,
+    1_200,
+)
