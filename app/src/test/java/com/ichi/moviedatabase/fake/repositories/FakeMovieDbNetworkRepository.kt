87fb743ed8fc305107b412658c98765199e035a1
Bastien MUNOZ <bastien.munoz@outlook.com>
2023-03-19 13:16:32 +0100

    Return new fake data in repository for movie

--- app/src/test/java/com/ichi/moviedatabase/fake/repositories/FakeMovieDbNetworkRepository.kt
+++ app/src/test/java/com/ichi/moviedatabase/fake/repositories/FakeMovieDbNetworkRepository.kt
@@ -1,12 +1,12 @@
 package com.ichi.moviedatabase.fake.repositories
 
-import com.ichi.moviedatabase.fake.data.fakeWSMovie
+import com.ichi.moviedatabase.fake.data.fakeMovie
 import com.ichi.moviedatabase.network.models.WSMovie
 import com.ichi.moviedatabase.network.models.WSMovies
 import com.ichi.moviedatabase.network.repositories.MovieDbNetworkRepositoryInterface
 
 class FakeMovieDbNetworkRepository : MovieDbNetworkRepositoryInterface {
-    private val movies = mutableListOf<WSMovies.Movie>()
+    private val movies: MutableList<WSMovies.Movie> = mutableListOf()
 
     fun addMovie(movie: WSMovies.Movie) {
         movies.add(movie)
@@ -21,6 +21,6 @@ class FakeMovieDbNetworkRepository : MovieDbNetworkRepositoryInterface {
     }
 
     override suspend fun getMovie(movieId: Int): WSMovie {
-        return fakeWSMovie
+        return fakeMovie
     }
 }
